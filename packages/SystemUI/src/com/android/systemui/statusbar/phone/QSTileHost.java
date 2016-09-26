5de483f11052bc6249c252378430d3a618a60be9
Altaf-Mahdi <altaf.mahdi@gmail.com>
2016-09-26 04:10:28 +0200

    [QUICK SETTINGS] Add Sync QS Tile
    * 3rd tile without CM's implementation. It's pretty easy after all. Credits to the original author of the commits below and zephiK.
    * Adapted for marshmallow by @KreAch3R
    * SystemUI Tuner configurable
    
    This is a combination of 2 commits.
    
    QS: add Sync tile (1/2)
    
    QS: Sync tile cleanup (1/2)
    
    * (Get inline with cm)
    * Add vector drawables (thanks to cm)
    
    fix sync
    
    fix sync
    
    Change-Id: I1c98c9d306a204aef436e806fcd39f7540967413
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	packages/SystemUI/AndroidManifest.xml
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/res/values/screwd_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -66,6 +66,7 @@ import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.NavigationBarTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.VolumeTile;
+import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
@@ -459,6 +460,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("nfc")) return new NfcTile(this);
         else if (tileSpec.equals("compass")) return new CompassTile(this);
         else if (tileSpec.equals("high_brightness") && mHighBrightnessSupported) return new HighBrightnessTile(this);
+        else if (tileSpec.equals("sync")) return new SyncTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
